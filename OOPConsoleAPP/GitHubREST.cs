120e55a77f31a6fb12818527a238d6241e9ff87d
Pagoong <obnialjerome@gmail.com>
2024-11-22 00:10:29 +0000

    http

--- OOPConsoleAPP/GitHubREST.cs
+++ OOPConsoleAPP/GitHubREST.cs
@@ -6,7 +6,35 @@ using System.Threading.Tasks;
 
 namespace OOPConsoleAPP
 {
-    internal class GitHubREST
+
+    //class declaration
+    public class GitHubREST
     {
+
+        //fields
+        private readonly HttpClient _httpClient;
+
+
+        //properties - Not required since we dont need to get or set
+
+        //constructor
+
+        public GitHubREST(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        //Methods - Method overloading
+
+        public async Task<string> GetGitHubAPI(string baseUrl)
+        {
+            var response = await _httpClient.GetAsync(baseUrl);
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadAsStringAsync();
+        }
+
+
+
+
     }
 }
